a9ade444e5d41039559d1fb8d3d76b0e9e271dee
Lucas Develay <lucas@test.com>
2024-08-09 15:15:11 +0200

    update font text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app.soccer_stats/main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +30,14 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF2F70AF),
-        title: const Text(
-          'Soccer Stats',
-          style: TextStyle(
-            color: Colors.white,
-            fontFamily: 'Fira Sans',
+        title: Text(
+            'Soccer Stats',
+            style: GoogleFonts.firaSans(
+              textStyle: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
           ),
-        ),
         leading: Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: Image.asset(
